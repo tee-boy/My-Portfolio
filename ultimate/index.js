5368831181e175b82bf26075c66bed2234ba42a4
TochukwuEmmanuel <tochukwuemmanuel690@gmail.com>
2024-08-06 21:12:29 +0100

    updates

--- ultimate/index.js
+++ ultimate/index.js
@@ -1,1 +1,21 @@
-document.getElementById("demo")
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+//   alert("Hello World!")
